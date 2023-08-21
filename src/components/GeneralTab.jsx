c821bfd1c8d313eef8c5c1f0c07b228aed272c9e
Kostadin Stanev <stanebvns@gmail.com>
2023-08-21 20:00:43 +0300

    minor code edits

--- src/components/GeneralTab.jsx
+++ src/components/GeneralTab.jsx
@@ -1,34 +1,37 @@
 import { forwardRef } from 'react';
 import './GeneralTab.css';
-const GeneralTab = forwardRef((props, ref) => {
+const GeneralTab = forwardRef((_props, ref) => {
   return (
-    <div role="tabpanel">
-      <form className="general-tab-form">
-        <label htmlFor="name">Name: </label>
-        <input placeholder="Test" id="name" ref={ref}></input>
-        <label htmlFor="email">Email: </label>
-        <input placeholder="Test" id="email"></input>
-        <div className="general-tab-buttons-group">
+    <div role='tabpanel'>
+      <form
+        className='general-tab-form'
+        aria-label='accessibility-enriched label'
+      >
+        <label htmlFor='name'>Name: </label>
+        <input placeholder='Test' id='name' ref={ref}></input>
+        <label htmlFor='email'>Email: </label>
+        <input placeholder='Test' id='email'></input>
+        <div className='general-tab-buttons-group'>
           <button>Save</button>
           <button>Cancel</button>
         </div>
       </form>
-      <form className="general-tab-form">
-        <label htmlFor="name">Name: </label>
-        <input placeholder="Test" id="name"></input>
-        <label htmlFor="email">Email: </label>
-        <input placeholder="Test" id="email"></input>
-        <div className="general-tab-buttons-group">
+      <form className='general-tab-form'>
+        <label htmlFor='name'>Name: </label>
+        <input placeholder='Test' id='name'></input>
+        <label htmlFor='email'>Email: </label>
+        <input placeholder='Test' id='email'></input>
+        <div className='general-tab-buttons-group'>
           <button>Save</button>
           <button>Cancel</button>
         </div>
       </form>
-      <form className="general-tab-form">
-        <label htmlFor="name">Name: </label>
-        <input placeholder="Test" id="name"></input>
-        <label htmlFor="email">Email: </label>
-        <input placeholder="Test" id="email"></input>
-        <div className="general-tab-buttons-group">
+      <form className='general-tab-form'>
+        <label htmlFor='name'>Name: </label>
+        <input placeholder='Test' id='name'></input>
+        <label htmlFor='email'>Email: </label>
+        <input placeholder='Test' id='email'></input>
+        <div className='general-tab-buttons-group'>
           <button>Save</button>
           <button>Cancel</button>
         </div>
